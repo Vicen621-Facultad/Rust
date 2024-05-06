acfba688511778b6489ce82e457104807c7bd36d
Vicen621 <71749981+vicen621@users.noreply.github.com>
2024-05-06 16:54:07 -0300

    Terminada practica 3

--- src/practica3/ej10.rs
+++ src/practica3/ej10.rs
@@ -9,12 +9,13 @@ struct Biblioteca {
     prestamos: Vec<Prestamo>,
 }
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Debug, Clone)]
 enum EstadoPrestamo {
     Devuelto,
     EnPrestamo
 }
 
+#[derive(PartialEq, Eq, Debug, Clone)]
 struct Prestamo {
     libro: Libro,
     cliente: Cliente,
@@ -39,7 +40,7 @@ impl Prestamo {
     }
 }
 
-#[derive(PartialEq, Eq, Clone)]
+#[derive(PartialEq, Eq, Clone, Debug)]
 struct Cliente {
     nombre: String,
     telefono: String,
@@ -112,13 +113,15 @@ impl Biblioteca {
         }
     }
 
-    fn incrementar_cantidad_copias(&mut self, libro: &Libro) {
-        let find_libro = self.libros.get_mut(libro);
+    fn incrementar_cantidad_copias(&mut self, libro: Libro) {
+        let find_libro = self.libros.get_mut(&libro);
 
         match find_libro {
             Some(cant) => *cant += 1,
-            None => {}
-        }
+            None => {
+                self.libros.insert(libro, 1);
+            },
+        };
     }
 
     fn decrementar_cantidad_copias(&mut self, libro: &Libro) {
@@ -190,8 +193,8 @@ impl Biblioteca {
         self.prestamos.iter_mut().find(|prestamo| &prestamo.libro == libro && &prestamo.cliente == cliente)
     }
 
-    fn devolver_libro(&mut self, libro: &Libro, cliente: &Cliente, fecha_actual: Fecha) {
-        let buscar_prestamo = self.buscar_prestamo(libro, cliente);
+    fn devolver_libro(&mut self, libro: Libro, cliente: &Cliente, fecha_actual: Fecha) {
+        let buscar_prestamo = self.buscar_prestamo(&libro, cliente);
 
         match buscar_prestamo {
             Some(prestamo) => {
@@ -205,3 +208,132 @@ impl Biblioteca {
 }
 
 //TODO: Hacer Tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_vencio_prestamo() {
+        let fecha_vencimiento = Fecha::new(10, 5, 2024);
+        let fecha_actual = Fecha::new(11, 5, 2024);
+        let prestamo = Prestamo::new(
+            Libro::new("Harry Potter".to_string(), "J.K. Rowling".to_string(), 300, Genero::Novela),
+            Cliente::new("John Doe".to_string(), "123456789".to_string(), "john@example.com".to_string()),
+            fecha_vencimiento,
+            None,
+            EstadoPrestamo::EnPrestamo
+        );
+
+        assert!(prestamo.vencio(&fecha_actual));
+    }
+
+    #[test]
+    fn test_no_vencio_prestamo() {
+        let fecha_vencimiento = Fecha::new(10, 5, 2024);
+        let fecha_actual = Fecha::new(9, 5, 2024);
+        let prestamo = Prestamo::new(
+            Libro::new("Harry Potter".to_string(), "J.K. Rowling".to_string(), 300, Genero::Novela),
+            Cliente::new("John Doe".to_string(), "123456789".to_string(), "john@example.com".to_string()),
+            fecha_vencimiento,
+            None,
+            EstadoPrestamo::EnPrestamo
+        );
+
+        assert!(!prestamo.vencio(&fecha_actual));
+    }
+
+    #[test]
+    fn test_realizar_prestamo_exitoso() {
+        let mut biblioteca = Biblioteca::new("Biblioteca".to_string(), "Calle Principal".to_string(), None, None);
+        let cliente = Cliente::new("John Doe".to_string(), "123456789".to_string(), "john@example.com".to_string());
+        let libro = Libro::new("Harry Potter".to_string(), "J.K. Rowling".to_string(), 300, Genero::Novela);
+        let fecha_devolucion = Fecha::new(20, 5, 2024);
+
+        biblioteca.incrementar_cantidad_copias(libro.clone());
+
+        assert!(biblioteca.realizar_prestamo(libro.clone(), cliente.clone(), fecha_devolucion));
+        assert_eq!(biblioteca.obtener_cantidad_copias(&libro), 0); // Se decrementó la cantidad de copias
+        assert_eq!(biblioteca.prestamos.len(), 1); // Se agregó un préstamo
+    }
+
+    #[test]
+    fn test_contar_prestamos_cliente() {
+        let mut biblioteca = Biblioteca::new("Biblioteca".to_string(), "Calle Principal".to_string(), None, None);
+        let cliente = Cliente::new("John Doe".to_string(), "123456789".to_string(), "john@example.com".to_string());
+        let libro1 = Libro::new("Libro1".to_string(), "Autor1".to_string(), 100, Genero::Novela);
+        let libro2 = Libro::new("Libro2".to_string(), "Autor2".to_string(), 200, Genero::Infantil);
+        let fecha_devolucion = Fecha::new(20, 5, 2024);
+
+        biblioteca.incrementar_cantidad_copias(libro1.clone());
+        biblioteca.incrementar_cantidad_copias(libro2.clone());
+
+        // Se realiza un préstamo
+        biblioteca.realizar_prestamo(libro1.clone(), cliente.clone(), fecha_devolucion.clone());
+        assert_eq!(biblioteca.obtener_cantidad_copias(&libro1), 0);
+        // Se cuenta el préstamo realizado
+        assert_eq!(biblioteca.contar_prestamos_cliente(&cliente), 1);
+
+        // Se realiza otro préstamo
+        biblioteca.realizar_prestamo(libro2.clone(), cliente.clone(), fecha_devolucion);
+        assert_eq!(biblioteca.obtener_cantidad_copias(&libro2), 0);
+        // Se cuenta el segundo préstamo realizado
+        assert_eq!(biblioteca.contar_prestamos_cliente(&cliente), 2);
+    }
+
+    #[test]
+    fn test_obtener_prestamos_vencidos() {
+        let mut biblioteca = Biblioteca::new("Biblioteca".to_string(), "Calle Principal".to_string(), None, None);
+        let cliente = Cliente::new("John Doe".to_string(), "123456789".to_string(), "john@example.com".to_string());
+        let libro = Libro::new("Harry Potter".to_string(), "J.K. Rowling".to_string(), 300, Genero::Novela);
+        let fecha_vencimiento = Fecha::new(20, 5, 2023);
+        let fecha_actual = Fecha::new(21, 5, 2023);
+        let prestamo_vencido = Prestamo::new(libro.clone(), cliente.clone(), fecha_vencimiento, None, EstadoPrestamo::EnPrestamo);
+
+        assert_eq!(biblioteca.obtener_prestamos_vencidos(&fecha_actual), vec![] as Vec<&Prestamo>);
+        biblioteca.prestamos.push(prestamo_vencido.clone());
+        assert_eq!(biblioteca.obtener_prestamos_vencidos(&fecha_actual), vec![&prestamo_vencido]);
+    }
+
+    #[test]
+    fn test_obtener_prestamos_a_vencer() {
+        let mut biblioteca = Biblioteca::new("Biblioteca".to_string(), "Calle Principal".to_string(), None, None);
+        let fecha_actual = Fecha::new(19, 5, 2023);
+        let cliente = Cliente::new("John Doe".to_string(), "123456789".to_string(), "john@example.com".to_string());
+        let libro = Libro::new("Harry Potter".to_string(), "J.K. Rowling".to_string(), 300, Genero::Novela);
+        let fecha_vencimiento = Fecha::new(20, 5, 2023);
+        let prestamo_a_vencer = Prestamo::new(libro.clone(), cliente.clone(), fecha_vencimiento, None, EstadoPrestamo::EnPrestamo);
+
+        assert_eq!(biblioteca.obtener_prestamos_a_vencer(fecha_actual.clone(), 2), vec![] as Vec<&Prestamo>);
+        biblioteca.prestamos.push(prestamo_a_vencer.clone());
+        assert_eq!(biblioteca.obtener_prestamos_a_vencer(fecha_actual, 2), vec![&prestamo_a_vencer]);
+    }
+
+    #[test]
+    fn test_buscar_prestamo() {
+        let mut biblioteca = Biblioteca::new("Biblioteca".to_string(), "Calle Principal".to_string(), None, None);
+        let cliente = Cliente::new("John Doe".to_string(), "123456789".to_string(), "john@example.com".to_string());
+        let libro = Libro::new("Harry Potter".to_string(), "J.K. Rowling".to_string(), 300, Genero::Novela);
+        let fecha_devolucion = Fecha::new(20, 5, 2024);
+        biblioteca.incrementar_cantidad_copias(libro.clone());
+        biblioteca.realizar_prestamo(libro.clone(), cliente.clone(), fecha_devolucion);
+
+        assert!(biblioteca.buscar_prestamo(&libro, &cliente).is_some());
+    }
+
+    #[test]
+    fn test_devolver_libro() {
+        let mut biblioteca = Biblioteca::new("Biblioteca".to_string(), "Calle Principal".to_string(), None, None);
+        let cliente = Cliente::new("John Doe".to_string(), "123456789".to_string(), "john@example.com".to_string());
+        let libro = Libro::new("Harry Potter".to_string(), "J.K. Rowling".to_string(), 300, Genero::Novela);
+        let fecha_devolucion = Fecha::new(20, 5, 2024);
+        biblioteca.incrementar_cantidad_copias(libro.clone());
+        biblioteca.realizar_prestamo(libro.clone(), cliente.clone(), fecha_devolucion);
+
+        // Se devuelve el libro
+        biblioteca.devolver_libro(libro.clone(), &cliente, Fecha::new(21, 5, 2024));
+        // Se incrementa la cantidad de copias
+        assert_eq!(biblioteca.obtener_cantidad_copias(&libro), 1);
+        // Se marca el préstamo como devuelto
+        assert_eq!(biblioteca.prestamos[0].estado, EstadoPrestamo::Devuelto);
+    }
+}
